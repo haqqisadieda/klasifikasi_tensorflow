3338c33e41e8b86979c2fce4c40281385035df1e
pandegahaes <haqqisadiedapandega@gmail.com>
2021-06-13 12:27:44 +0700

    add comment

--- classification.py
+++ classification.py
@@ -5,15 +5,21 @@ import tensorflow as tf
 
 from tensorflow import keras
 
+#Memanggil base directory project
 BASE_DIR = BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+#Memanggil directory model dan dataset
 data_model = BASE_DIR + "/model"
 dataset_dir = BASE_DIR + "/dataset"
+
+#Mengambil semua dataset
 data_dir = pathlib.Path(dataset_dir)
 
+#Menentukan batch size dan size gambar
 batch_size = 32
 size = 180
 
+#Memecah data untuk digunakan sebagai data latih sebanyak 80%
 train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     data_dir,
     validation_split=0.2,
@@ -23,6 +29,7 @@ train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     batch_size=batch_size
 )
 
+#Memecah data untuk digunakan sebagai validasi data sebanyak 20%
 val_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     data_dir,
     validation_split=0.2,
@@ -32,21 +39,27 @@ val_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     batch_size=batch_size
 )
 
+#Menentukan nama setiap klasifikasi dari nama klasifikasi yang diambil dari data latih
 class_names = train_dataset.class_names
 
+#Pengambilan jumlah klasifikasi dari banyaknya nama klasifikasi
 num_classes = len(class_names)
 
+#Proses load data model
 model = keras.models.load_model(data_model)
 
+#Mengambil data gambar yang akan ditest klasifikasinya
 img_pred_dir = BASE_DIR + '/prediction/dandelion.jpg'
 
 img_pred = keras.preprocessing.image.load_img(
     img_pred_dir, target_size=(size, size)
 )
 
+#Merubah data gambar ke array
 img_pred_array = keras.preprocessing.image.img_to_array(img_pred)
 img_pred_array = tf.expand_dims(img_pred_array, 0)
 
+#Proses klasifikasi
 prediction = model.predict(img_pred_array)
 score = tf.nn.softmax(prediction[0])
 
--- train_dataset.py
+++ train_dataset.py
@@ -7,15 +7,21 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
+#Memanggil base directory project
 BASE_DIR = BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+#Memanggil directory model dan dataset
 data_model = BASE_DIR + "/model"
 dataset_dir = BASE_DIR + "/dataset"
+
+#Mengambil semua dataset
 data_dir = pathlib.Path(dataset_dir)
 
+#Menentukan batch size dan size gambar
 batch_size = 32
 size = 180
 
+#Memecah data untuk digunakan sebagai data latih sebanyak 80%
 train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     data_dir,
     validation_split=0.2,
@@ -25,6 +31,7 @@ train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     batch_size=batch_size
 )
 
+#Memecah data untuk digunakan sebagai validasi data sebanyak 20%
 val_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     data_dir,
     validation_split=0.2,
@@ -34,8 +41,10 @@ val_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     batch_size=batch_size
 )
 
+#Menentukan nama setiap klasifikasi dari nama klasifikasi yang diambil dari data latih
 class_names = train_dataset.class_names
 
+#Normalisasi data latih
 AUTOTUNE = tf.data.AUTOTUNE
 
 train_dataset = train_dataset.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
@@ -47,8 +56,10 @@ normalized_dataset = train_dataset.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_dataset))
 first_image = image_batch[0]
 
+#Pengambilan jumlah klasifikasi dari banyaknya nama klasifikasi
 num_classes = len(class_names)
 
+#Pembuatan model dataset
 model = Sequential([
     layers.experimental.preprocessing.Rescaling(1./255, input_shape=(size, size, 3)),
     layers.Conv2D(16, 3, padding='same', activation='relu'),
@@ -62,21 +73,27 @@ model = Sequential([
     layers.Dense(num_classes)    
 ])
 
+#Kompilasi model
 model.compile(
     optimizer='adam',
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     metrics=['accuracy']
 )
 
+#Menampilkan model yang sudah dikompilasi 
 model.summary()
 
+#Menentukan jumlah epoch, satu epoch adalah ketika seluruh dataset terlewati baik forward ataupun backward melalui jaringan syaraf 
 epochs = 10
+
+#Proses training
 history = model.fit(
     train_dataset,
     validation_data=val_dataset,
     epochs=epochs
 )
 
+#Proses menampilkan grafik
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
@@ -85,6 +102,7 @@ val_loss = history.history['val_loss']
 
 epochs_range = range(epochs)
 
+#Grafik training and validation accuracy
 plt.figure(figsize=(8, 8))
 plt.subplot(1, 2, 1)
 plt.plot(epochs_range, acc, label='Training Accuracy')
@@ -92,6 +110,7 @@ plt.plot(epochs_range, val_acc, label='Validation Accuracy')
 plt.legend(loc='lower right')
 plt.title('Training and Validation Accuracy')
 
+#Grafik training and validation loss
 plt.subplot(1, 2, 2)
 plt.plot(epochs_range, loss, label='Training Loss')
 plt.plot(epochs_range, val_loss, label='Validation Loss')
@@ -99,6 +118,7 @@ plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
 
+#Augmentasi
 data_augmentation = keras.Sequential(
     [
         layers.experimental.preprocessing.RandomFlip("horizontal",
@@ -110,6 +130,7 @@ data_augmentation = keras.Sequential(
     ]
 )
 
+#Pembuatan model data augmentasi dan dropout
 model = Sequential([
     data_augmentation,
     layers.experimental.preprocessing.Rescaling(1./255),
@@ -125,14 +146,17 @@ model = Sequential([
     layers.Dense(num_classes)
 ])
 
+#Kompilasi model
 model.compile(
     optimizer='adam',
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     metrics=['accuracy']
 )
 
+#Menampilkan model yang sudah dikompilasi
 model.summary()
 
+#Menentukan jumlah epoch
 epochs = 15
 history = model.fit(
     train_dataset,
@@ -140,6 +164,7 @@ history = model.fit(
     epochs=epochs
 )
 
+#Proses menampilkan grafik
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
@@ -148,6 +173,7 @@ val_loss = history.history['val_loss']
 
 epochs_range = range(epochs)
 
+#Grafik training and validation accuracy
 plt.figure(figsize=(8, 8))
 plt.subplot(1, 2, 1)
 plt.plot(epochs_range, acc, label='Training Accuracy')
@@ -155,6 +181,7 @@ plt.plot(epochs_range, val_acc, label='Validation Accuracy')
 plt.legend(loc='lower right')
 plt.title('Training and Validation Accuracy')
 
+#Grafik training and validation loss
 plt.subplot(1, 2, 2)
 plt.plot(epochs_range, loss, label='Training Loss')
 plt.plot(epochs_range, val_loss, label='Validation Loss')
@@ -162,6 +189,7 @@ plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
 
+#Proses penyimpanan model
 if not os.path.exists(data_model):
   os.makedirs(data_model)
 
